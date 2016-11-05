02a936677f2a52649c8976fa5d8f1fd0bd251495
vihangpatel <vihang.engg@gmail.com>
2016-11-05 13:55:12 +0530

    Make server to work

--- server.js
+++ server.js
@@ -1,2 +1,18 @@
 var express = require("express");
 var app     = express();
+
+// Everything should be in constant
+// If your port is occupied then server will throw an error
+// In that case change the following variable
+var appPort = 3000;
+
+
+// Add route to the server
+app.get('/',function(req,res){
+ 	res.send('Yoooo My server worked')
+});
+
+// Listen on
+app.listen(appPort);
+
+console.log("Running at Port " + appPort);
