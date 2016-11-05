d5161f5b6f6b73d41f78fc188cf67a857737bf6a
vihangpatel <vihang.engg@gmail.com>
2016-11-05 17:18:20 +0530

    Implement JS file

---
+++ app/js/chat.js
@@ -0,0 +1,7 @@
+$(document).ready(main);
+
+function main() {
+	console.log('Adding main file which will handle ');
+	console.warn('Warning is written this way');
+	console.info('Information is written this way');
+}
