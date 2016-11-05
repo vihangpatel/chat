c89eee28acafe19317302d8f27ad01944e1223e9
vihangpatel <vihang.engg@gmail.com>
2016-11-05 17:42:39 +0530

    Listen socket connection on the server

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 var express = require("express");
 var app     = express();
 
+var http = require('http')
+  , server = http.createServer(app)
+  , io = require('socket.io').listen(server);
+
 // Everything should be in constant
 // If your port is occupied then server will throw an error
 // In that case change the following variable
@@ -8,7 +12,7 @@ var appPort = 3000;
 
 // Configure the static path
 app.use(express.static(__dirname + '/app/templates'));
-app.use('/scripts/vendor', express.static(__dirname + '/node_modules/jquery/dist/'));
+app.use('/scripts/vendor', express.static(__dirname + '/node_modules/'));
 app.use('/scripts/js', express.static(__dirname + '/app/js'));
 app.use('/styles/css', express.static(__dirname + '/app/css'));
 
@@ -17,7 +21,11 @@ app.get('/',function(req,res){
  	res.sendFile('index.html')
 });
 
+io.sockets.on('connection', function (socket) { // First connection
+	console.log('connection arrived')
+})
+
 // Listen on
-app.listen(appPort);
+server.listen(appPort);
 
 console.log("Running at Port " + appPort);
